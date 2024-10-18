2530ba171a2cf6f424d507ded7edeb3edf9f7362
Victor Peinado <vpeinado263@gmail.com>
2024-10-18 18:31:08 -0300

    correcion en el evento click

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.getElementById('addNoteBtn').addEventListener('click', addNoteBtn);
+
+function addNote() {
+    const note = document.createElement('div');
+    note.className = 'note';
+    note.contentEditable = true;
+    note.innerText = 'Nueva Nota';
+    positionNote(note);
+    document.getElementById('board').appendChild(note);
+    makeDraggable(note);
+}
