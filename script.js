52b8a56ec698358af900ce64d55b7dd0d2a7c184
Victor Peinado <vpeinado263@gmail.com>
2024-10-18 18:58:47 -0300

    se crea function makeDraggable

--- script.js
+++ script.js
@@ -10,6 +10,32 @@ function addNote() {
     makeDraggable(note);
 }
 function positionNote(note) {
-    note.style.left = Math.random() * 80 + 'vw';
-    note.style.top = Math.random() * 80 + 'vw';
+    note.style.left = Math.random() * 80 + 'vh';
+    note.style.top = Math.random() * 80 + 'vh';
+}
+function makeDraggable(note) {
+    note.onmousedown = function(event) {
+        let shiftX = event.clientX - note.getBoudingClientRect().left;
+        let shiftY = event.clientY - note.getBoudingClientRect().top;
+
+        function moveAt(pageX, pageY) {
+            note.style.left = pageX - shiftX + 'px';
+            note.style.top = pageY - shiftY + 'px';
+        }
+
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        note.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            note.onmouseup = null;
+        };
+    };
+
+    note.ondragstart = function() {
+        return false;
+    };
 }
